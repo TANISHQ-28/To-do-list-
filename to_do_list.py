dffb45d3adbc85629ebc42a889ce2ed6970f5e63
TANISHQ-28 <tanishqsoni2807@gmail.com>
2026-08-30 22:14:00 +0530

    Implement file I/O for task management
    Added functionality to save tasks to a file and load tasks from a file.

--- to_do_list.py
+++ to_do_list.py
@@ -1,13 +1,17 @@
 def viewtask(a):
   if len(a)==0:
     print("No tasks available")
+    return
   for i in range(1,len(a)+1):
      print(f"{i}. {a[i-1]}")
-  
 
 def addtask(a):
   n = input("\nEnter the task you want to add : ")
   a.append(n)
+
+  with open("tasks.txt", "a") as f:
+    f.write(n + "\n")
+
 
 def deletetask(a):
   try :
@@ -17,33 +21,39 @@ def deletetask(a):
     if n<1 or n>len(a):
       print("Value out of Bound!")
 
-    for i in range(1, len(a)+1):
-      if (i-1) == n-1:
-        b = a.pop(i-1)
+    b = a.pop(n-1)
+    with open("tasks.txt", "w") as f:
+      for task in a:
+        f.write(task + "\n")
+      
     print(f"\nThe deleted task is '{b}'\nTask deleted Successfuly.")
 
   except ValueError:
     print("\nValueError occurred!\nEnter a valid integer value.")
 
-
 #use while loop then only addition problem will be solved.
 print("Your tasks are stored pefectly in this program.")
 print("To add, delete and view your tasks press :\n1. To view tasks\n2. To add a new task\n3. To delete a task\n4. To exit the program")
-l1 = ["Python Project","Hit the gym", "Meditate"]
+
+with  open("tasks.txt", "r") as f:
+  task = f.read()
+# print(repr(task))
+s = task.splitlines()
+t = list(s)
 
 while True:
   try:
     a = int(input("\nEnter the choice : "))
 
     if a == 1: 
-      viewtask(l1)
+      viewtask(t)
 
     elif a == 2:
-      addtask(l1)
+      addtask(t)
       print("\n")
 
     elif a == 3:
-      deletetask(l1)
+      deletetask(t)
   
     elif a == 4:
       print("\nExiting the program......\nExited Successfuly!")
@@ -54,5 +64,3 @@ while True:
     
   except ValueError:
     print("ValueError occurred!\nEnter a valid integer value.")
-
-
